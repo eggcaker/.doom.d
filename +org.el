eda68d9091137773f58e647a4543e76f9a51fee0
eggcaker <eggcaker@gmail.com>
2020-02-24 15:20:25 +0800

    update font setup

--- +org.el
+++ +org.el
@@ -5,3 +5,77 @@
   (setq org-crypt-tag-matcher "secret")
   (setq org-tags-exclude-from-inheritance (quote ("secret")))
   (setq org-crypt-key "CF0B552FF"))
+
+
+;; latex for chinese
+(after! latex
+  (add-to-list 'org-latex-classes '("article" "\\documentclass[a4paper,11pt]{article}
+
+        [NO-DEFAULT-PACKAGES]
+          \\usepackage[utf8]{inputenc}
+          \\usepackage[T1]{fontenc}
+          \\usepackage{fixltx2e}
+          \\usepackage{graphicx}
+          \\usepackage{longtable}
+          \\usepackage{float}
+          \\usepackage{wrapfig}
+          \\usepackage{rotating}
+          \\usepackage[normalem]{ulem}
+          \\usepackage{amsmath}
+          \\usepackage{textcomp}
+          \\usepackage{marvosym}
+          \\usepackage{wasysym}
+          \\usepackage{amssymb}
+          \\usepackage{booktabs}
+          \\usepackage[colorlinks,linkcolor=black,anchorcolor=black,citecolor=black]{hyperref}
+          \\tolerance=1000
+          \\usepackage{listings}
+          \\usepackage{xcolor}
+          \\usepackage{fontspec}
+          \\usepackage{xeCJK}
+          \\setCJKmainfont{Weibei SC}
+          \\setmainfont{Fantasque Sans Mono}
+          \\lstset{
+          %行号
+          numbers=left,
+          %背景框
+          framexleftmargin=10mm,
+          frame=none,
+          %背景色
+          %backgroundcolor=\\color[rgb]{1,1,0.76},
+          backgroundcolor=\\color[RGB]{245,245,244},
+          %样式
+          keywordstyle=\\bf\\color{blue},
+          identifierstyle=\\bf,
+          numberstyle=\\color[RGB]{0,192,192},
+          commentstyle=\\it\\color[RGB]{0,96,96},
+          stringstyle=\\rmfamily\\slshape\\color[RGB]{128,0,0},
+          %显示空格
+          showstringspaces=false
+          }
+          "
+                                    ("\\section{%s}" . "\\section*{%s}")
+                                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  ;; {{ export org-mode in Chinese into PDF
+  ;; @see http://freizl.github.io/posts/tech/2012-04-06-export-orgmode-file-in-Chinese.html
+  ;; and you need install texlive-xetex on different platforms
+  ;; To install texlive-xetex:
+  ;;    `sudo USE="cjk" emerge texlive-xetex` on Gentoo Linux
+  ;; }}
+  ;;(setq org-latex-default-class "ctexart")
+  (add-to-list 'org-latex-packages-alist '("" "minted"))
+  (setq org-latex-listings 'minted)
+  (setq org-src-fontify-natively t)
+  (setq org-latex-pdf-process
+        '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+          "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+          "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+          "xelatex -interaction nonstopmode -output-directory %o %f"
+          "xelatex -interaction nonstopmode -output-directory %o %f"
+          "xelatex -interaction nonstopmode -output-directory %o %f"
+          "rm -fr %b.out %b.log %b.tex auto"))
+)
--- +ui.el
+++ +ui.el
@@ -5,9 +5,26 @@
 (setq doom-theme 'doom-vibrant)
 
 ;; fonts
-(setq doom-font (font-spec :family "Iosevka Term Medium" :size 18))
-(setq doom-variable-pitch-font (font-spec :family "Noto Sans" :weight 'semi-bold :width 'extra-condensed))
-(setq doom-serif-font (font-spec :family "Noto Serif" :weight 'semi-bold :width 'extra-condensed))
+;; (setq doom-font (font-spec :family "Iosevka Term Medium" :size 18))
+;; (setq doom-variable-pitch-font (font-spec :family "Noto Sans" :weight 'semi-bold :width 'extra-condensed))
+;; (setq doom-serif-font (font-spec :family "Noto Serif" :weight 'semi-bold :width 'extra-condensed))
+
+(set-face-attribute
+ 'default nil
+ :font (font-spec :name "-*-Fantasque Sans Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"
+                  :weight 'normal
+                  :slant 'normal
+                  :size 20.0))
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   charset
+   (font-spec :name "-*-STFangsong-normal-normal-normal-*-*-*-*-*-p-0-iso10646-1"
+              :weight 'normal
+              :slant 'normal
+              :size 19.6)))
+
+(setq ispell-program-name "/usr/local/bin/ispell")
 
 ;; line spacing
 (setq display-line-numbers-type nil)
