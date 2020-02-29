74aad56c0d652514c2e5957012708a02dc6f7ab4
eggcaker <eggcaker@gmail.com>
2020-02-29 22:05:03 +0800

    add magit popp rules

--- +git.el
+++ +git.el
@@ -13,6 +13,6 @@
   (set-popup-rule! "^\\*Magit" :slot -1  :side 'right :size  80 :modeline nil :select t)
   (set-popup-rule! "^\\*Magit Repositories\\*" :side 'bottom :size 50 :modeline nil :select nil)
   (set-popup-rule! "^\\*magit.*popup\\*" :slot 0 :side 'right  :modeline nil :select  t)
+  (set-popup-rule! "^\\*magit-gitflow.*popup\\*" :slot 0 :side 'bottom :size 0.25 :modeline nil :select  t)
   (set-popup-rule! "^\\*magit-revision:.*" :slot  0 :side  'right :window-height 0.6 :modeline  nil :select  t)
-  (set-popup-rule! "^\\*magit-diff:.*" :slot  0 :side 'right :window-height  0.6 :modeline nil :select nil)
-  (add-hook! 'magit-popup-mode-hook #'doom-hide-modeline-mode))
+  (set-popup-rule! "^\\*magit-diff:.*" :slot  0 :side 'right :window-height  0.6 :modeline nil :select nil))
