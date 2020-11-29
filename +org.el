075eb39e75d9a9d8f91661bb14a6c9bca43f1db3
eggcaker <eggcaker@gmail.com>
2020-11-29 18:02:59 +0800

    Daily commit 2020-11-29

--- +org.el
+++ +org.el
@@ -190,79 +190,6 @@
                 ("DONE" ("WAITING") ("CANCELLED")))))
   )
 
-;; latex for chinese
-(after! latex
-  (add-to-list 'org-latex-classes '("article" "\\documentclass[a4paper,11pt]{article}
-
-        [NO-DEFAULT-PACKAGES]
-          \\usepackage[utf8]{inputenc}
-          \\usepackage[T1]{fontenc}
-          \\usepackage{fixltx2e}
-          \\usepackage{graphicx}
-          \\usepackage{longtable}
-          \\usepackage{float}
-          \\usepackage{wrapfig}
-          \\usepackage{rotating}
-          \\usepackage[normalem]{ulem}
-          \\usepackage{amsmath}
-          \\usepackage{textcomp}
-          \\usepackage{marvosym}
-          \\usepackage{wasysym}
-          \\usepackage{amssymb}
-          \\usepackage{booktabs}
-          \\usepackage[colorlinks,linkcolor=black,anchorcolor=black,citecolor=black]{hyperref}
-          \\tolerance=1000
-          \\usepackage{listings}
-          \\usepackage{xcolor}
-          \\usepackage{fontspec}
-          \\usepackage{xeCJK}
-          \\setCJKmainfont{Weibei SC}
-          \\setmainfont{Fantasque Sans Mono}
-          \\lstset{
-          %行号
-          numbers=left,
-          %背景框
-          framexleftmargin=10mm,
-          frame=none,
-          %背景色
-          %backgroundcolor=\\color[rgb]{1,1,0.76},
-          backgroundcolor=\\color[RGB]{245,245,244},
-          %样式
-          keywordstyle=\\bf\\color{blue},
-          identifierstyle=\\bf,
-          numberstyle=\\color[RGB]{0,192,192},
-          commentstyle=\\it\\color[RGB]{0,96,96},
-          stringstyle=\\rmfamily\\slshape\\color[RGB]{128,0,0},
-          %显示空格
-          showstringspaces=false
-          }
-          "
-                                    ("\\section{%s}" . "\\section*{%s}")
-                                    ("\\subsection{%s}" . "\\subsection*{%s}")
-                                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
-  ;; {{ export org-mode in Chinese into PDF
-  ;; @see http://freizl.github.io/posts/tech/2012-04-06-export-orgmode-file-in-Chinese.html
-  ;; and you need install texlive-xetex on different platforms
-  ;; To install texlive-xetex:
-  ;;    `sudo USE="cjk" emerge texlive-xetex` on Gentoo Linux
-  ;; }}
-  ;;(setq org-latex-default-class "ctexart")
-  (add-to-list 'org-latex-packages-alist '("" "minted"))
-  (setq org-latex-listings 'minted)
-  (setq org-src-fontify-natively t)
-  (setq org-latex-pdf-process
-        '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-          "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-          "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-          "xelatex -interaction nonstopmode -output-directory %o %f"
-          "xelatex -interaction nonstopmode -output-directory %o %f"
-          "xelatex -interaction nonstopmode -output-directory %o %f"
-          "rm -fr %b.out %b.log %b.tex auto"))
-  )
-
 ;; org deft
 (setenv "XAPIAN_CJK_NGRAM" "1")
 (use-package! notdeft
@@ -353,11 +280,7 @@
   :after org
   )
 
-(add-hook! LaTeX-mode
-  (add-to-list TeX-command-list '("LuaLaTeX" "%`lualatex%(mode)%' %t" TeX-run-TeX nil t))
-  (setq TeX-command-default "LuaLaTeX"
-        TeX-save-query nil
-        TeX-show-compilation t))
+
 (use-package! ox-novel
   :after org
   )
