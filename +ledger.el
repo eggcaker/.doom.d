a27e4a0b44eed6b56c093b29e30a0f94e5d9afe6
eggcaker <eggcaker@gmail.com>
2022-02-10 21:13:13 +0800

    Auto commit daily work at 2022-02-10 21:13:13

--- +ledger.el
+++ +ledger.el
@@ -4,29 +4,29 @@
   :commands hledger-enable-reporting
   :preface
 
-(defun center-text-for-reading (&optional arg)
-  "Setup margins for reading long texts.
+  (defun center-text-for-reading (&optional arg)
+    "Setup margins for reading long texts.
 If ARG is supplied, reset margins and fringes to zero."
-  (interactive "P")
-  ;; Set the margin width to zero first so that the whole window is
-  ;; available for text area.
-  (set-window-margins (selected-window) 0)
-  (let* ((max-text-width (save-excursion
-                           (let ((w 0))
-                             (goto-char (point-min))
-                             (while (not (eobp))
-                               (end-of-line)
-                               (setq w (max w (current-column)))
-                               (forward-line))
-                             w)))
-         (margin-width (if arg
-                           0
-                         (/ (max (- (frame-width) max-text-width) 0) 3))))
-    (setq left-margin-width margin-width)
-    (setq right-margin-width margin-width)
-    ;; `set-window-margings' does a similar thing but those changes do
-    ;; not persist across buffer switches.
-    (set-window-buffer nil (current-buffer))))
+    (interactive "P")
+    ;; Set the margin width to zero first so that the whole window is
+    ;; available for text area.
+    (set-window-margins (selected-window) 0)
+    (let* ((max-text-width (save-excursion
+                             (let ((w 0))
+                               (goto-char (point-min))
+                               (while (not (eobp))
+                                 (end-of-line)
+                                 (setq w (max w (current-column)))
+                                 (forward-line))
+                               w)))
+           (margin-width (if arg
+                             0
+                           (/ (max (- (frame-width) max-text-width) 0) 3))))
+      (setq left-margin-width margin-width)
+      (setq right-margin-width margin-width)
+      ;; `set-window-margings' does a similar thing but those changes do
+      ;; not persist across buffer switches.
+      (set-window-buffer nil (current-buffer))))
 
 
   (defun hledger/next-entry ()
@@ -58,6 +58,18 @@ If ARG is supplied, reset margins and fringes to zero."
   ;; Expanded account balances in the overall monthly report are
   ;; mostly noise for me and do not convey any meaningful information.
   (setq hledger-show-expanded-report nil)
+
+  (setq-default hledger-currency-string "￥" )
+  (setq-default hledger-top-asset-account "资产")
+  (setq-default hledger-top-expense-account "支出")
+  (setq-default hledger-top-income-account "收入")
+  (setq-default hledger-year-of-birth 1979)
+  (setq-default hledger-daily-report-accounts "支出")
+  (setq-default hledger-ratios-assets-accounts "资产")
+  (setq-default hledger-ratios-income-accounts "收入")
+  (setq-default hledger-ratios-liquid-asset-accounts "活期 天添利 公积金 美元")
+  (setq-default hledger-ratios-essential-expense-accounts "支出:淘宝 支出:日常支出:买菜和日常用品 支出:日常支出:外出就餐 支出:日常支出:超市购物 支出:车:油费 支出:转帐:刘春兰")
+  (setq-default hledger-ratios-debt-accounts "信用卡")
   (when (boundp 'my-hledger-service-fetch-url)
     (setq hledger-service-fetch-url
           my-hledger-service-fetch-url))
