8421f57760ab65916b7003473a3b95c7f8b4b5bc
eggcaker <eggcaker@gmail.com>
2023-04-18 21:59:07 +0800

    add claude integration

--- +misc.el
+++ +misc.el
@@ -33,9 +33,45 @@
                           (thing-at-point 'line t)))
          (output    (shell-command-to-string (concat "wgpt " "`" current-line "`"))))
     (beginning-of-line)
-    (insert "GPT> ")
+    (insert "GPT>>> ")
     (end-of-line)
     (insert "\n\n")
-    (insert (mapconcat 'identity (nthcdr 5 (seq-filter (lambda (line) (not (string-suffix-p "Copy code" line))) (split-string output "\n"))) "\n"))))
+    (insert (mapconcat 'identity (nthcdr 5 (seq-filter (lambda (line) (not (string-suffix-p "Copy code" line))) (split-string output "\n"))) "\n"))
 
-(global-set-key (kbd "C-;") 'query-gpt-chat)
+    (insert "\n<<<GPT\n")))
+
+(global-set-key (kbd "C-:") 'query-gpt-chat)
+
+
+(defun query-claude-chat()
+  "Run script command with selected text or current line content and insert output in buffer"
+  (interactive)
+  (let* ((selected-text (if (region-active-p)
+                            (buffer-substring-no-properties (region-beginning) (region-end))
+                          nil))
+         (current-line (if selected-text
+                            (replace-regexp-in-string "\n" "POOR_GPT_SEP" selected-text)
+                          (thing-at-point 'line t)))
+         (output
+                     (shell-command-to-string (concat "claude " "`" current-line "`"))
+
+
+                   ))
+    (beginning-of-line)
+    (insert "GPT>>> ")
+    (end-of-line)
+    (insert "\n\n")
+    (if (not (string= output ""))
+    (insert (mapconcat 'identity (nthcdr 5 (seq-filter (lambda (line) (not (string-suffix-p "Copy code" line))) (split-string output "\n"))) "\n"))
+    (insert "\n<<<GPT\n")
+    )
+
+
+    ))
+
+(global-set-key (kbd "C-;") 'query-claude-chat)
+
+
+GPT>>>
+
+GPT>>>
